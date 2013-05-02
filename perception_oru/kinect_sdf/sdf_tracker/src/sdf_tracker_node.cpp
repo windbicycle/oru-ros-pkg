aa82f68f243db3b969fc1cf2640f59b5527a8661
danielcanelhas <danielcanelhas@468628a8-23b0-0fc3-096a-3fcb212faa4c>
2013-05-02 19:02:42 +0000

    Cmakelists accidentally got deleted

--- perception_oru/kinect_sdf/sdf_tracker/src/sdf_tracker_node.cpp
+++ perception_oru/kinect_sdf/sdf_tracker/src/sdf_tracker_node.cpp
@@ -280,6 +280,18 @@ int main( int argc, char* argv[] )
     ros::init(argc, argv, "sdf_tracker_node");
     
     SDF_Parameters param;
+
+     //Pose Offset as a transformation matrix
+    Eigen::Matrix4d initialTransformation = 
+    Eigen::MatrixXd::Identity(4,4);
+
+    //define translation offsets in x y z
+    initialTransformation(0,3) = 0.0;  //x 
+    initialTransformation(1,3) = 0.0;  //y
+    initialTransformation(2,3) = -0.7; //z
+
+    param.pose_offset = initialTransformation;
+
     SDFTrackerNode MyTrackerNode(param);
     MyTrackerNode.subscribeTopic();
     MyTrackerNode.advertiseTopic();
